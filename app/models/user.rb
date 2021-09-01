d414f5dc1159634721e59baa59cbdf34fa8ed4ba
natsumeryuichi <a031133@gmail.com>
2021-09-01 09:01:51 +0900

    room_usersテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ApplicationRecord
   # Userモデルに、validates :name, presence: trueを追記します。
   # 「name」カラムに、presence: trueを設けることで、空の場合はDBに保存しないというバリデーションを設定しています。
   # つまり、ユーザー登録時に「name」を空欄にして登録しようとすると、エラーが発生します。
+  has_many :room_users
+  has_many :rooms, through: :room_users
+# usersテーブルとroomsテーブルのアソシエーションを記述
 end
