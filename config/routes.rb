c39190a61c33153b9f2e40ccbeacd58fb01eccae
natsumeryuichi <a031133@gmail.com>
2021-09-03 07:38:51 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
-  root to: "messages#index"
+  get 'rooms/index'
+  root to: "rooms#index"
   resources :users, only: [:edit, :update]
 # ユーザー編集に必要なルーティングは、editとupdateなので、routes.rbに、 resources :users, only: [:edit, :update]と追記します。
 # 次に、rails g controller usersコマンドで編集機能の実装に必要なUsersコントローラーを作成します。
