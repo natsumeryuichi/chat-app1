9ba0186ce7fa1d55443d850d932190d3fad1b611
natsumeryuichi <a031133@gmail.com>
2021-09-03 07:24:40 +0900

    チャットルーム作成機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,11 +19,9 @@ class UsersController < ApplicationController
   end
 
   private
-
   def user_params
     params.require(:user).permit(:name, :email)
   # user_paramsの中でpermitメソッドを使用し、「name」と「email」の編集を許可します。
   # そして、ユーザー情報が格納されているcurrent_userメソッドを使用して、ログインしているユーザーの情報を更新します。
-
   end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,9 @@ class Room < ApplicationRecord
   has_many :room_users
   has_many :users, through: :room_users
 # usersテーブルとroomsテーブルのアソシエーションを記述
+
+  validates :name, presence: true
+  # チャットルームを新規作成するにあたって「ルーム名」は必ず必要なので、
+  # 上記のバリデーションは「ルーム名が存在（presence）している場合のみ作成可（true）」という意味です。
+
 end
