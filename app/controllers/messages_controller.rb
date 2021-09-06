0c135fd88f427593fac80e6653ed54f2903903a1
natsumeryuichi <a031133@gmail.com>
2021-09-06 17:13:07 +0900

    Active Storageの導入

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -36,7 +36,7 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:content).merge(user_id: current_user.id)
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
 # privateメソッドとしてmessage_paramsを定義し、メッセージの内容contentをmessagesテーブルへ保存できるようにします。
 # パラメーターの中に、ログインしているユーザーのidと紐付いている、メッセージの内容contentを受け取れるように許可します。
   end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,9 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
+  has_one_attached :image
+# 各レコードとファイルを1対1の関係で紐づけるメソッドです。
+# has_one_attachedメソッドを記述したモデルの各レコードは、それぞれ1つのファイルを添付できます。
 
   validates :content, presence: true
 end
