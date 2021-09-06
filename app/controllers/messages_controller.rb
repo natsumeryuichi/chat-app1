097d0f24f881946948b44716cdac9f6d53c59327
natsumeryuichi <a031133@gmail.com>
2021-09-06 15:02:01 +0900

    messagesテーブルの作成とメッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,32 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+#  @messageには、Message.newで生成した、Messageモデルのインスタンス情報を代入します。
+    @room = Room.find(params[:room_id])
+# @roomには、Room.find(params[:room_id])と記述することで、paramsに含まれているroom_idを代入します。
+# 紐解いて説明すると、直前の問題にあった通りルーティングをネストしているため/rooms/:room_id/messagesといったパスになります。
+# パスにroom_idが含まれているため、paramsというハッシュオブジェクトの中に、room_idという値が存在しています。
+# そのため、params[:room_id]と記述することでroom_idを取得できます。
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      render :index
+    end
+# messagesコントローラーにcreateアクションを定義します。
+# @room.messages.newでチャットルームに紐づいたメッセージのインスタンスを生成し、message_paramsを引数にして、privateメソッドを呼び出します。
+# 生成したインスタンスを@messageに代入し、saveメソッドでメッセージの内容をmessagesテーブルに保存します。
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
+# privateメソッドとしてmessage_paramsを定義し、メッセージの内容contentをmessagesテーブルへ保存できるようにします。
+# パラメーターの中に、ログインしているユーザーのidと紐付いている、メッセージの内容contentを受け取れるように許可します。
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update]
 # ユーザー編集に必要なルーティングは、editとupdateなので、routes.rbに、 resources :users, only: [:edit, :update]と追記します。
 # 次に、rails g controller usersコマンドで編集機能の実装に必要なUsersコントローラーを作成します。
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
 # 新規チャットルームの作成で動くアクションは「new」と「create」のみなので、上記のようなコードとなります。
+    resources :messages, only: [:index, :create]
+# 今回の場合、ネストをすることにより、roomsが親で、messagesが子という親子関係になるので、チャットルームに属しているメッセージという意味になります。
+# これによって、メッセージに結びつくルームのidの情報を含んだパスを、受け取れるようになります。
+  end
 end
